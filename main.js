405b7c165376e65aa797281ad002e540dbe15018
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-08-03 16:50:42 +0300

    add result
    add option of name, heighy, weight and a picture that show front of the pokemon and if you mouse over it show a back image.

--- main.js
+++ main.js
@@ -2,48 +2,35 @@ const searchInput = document.getElementById("search");
 const searchButton = document.getElementById("searchButton");
 const pokeDiv = document.getElementById("pokeDiv");
 
+searchButton.addEventListener("click", () => searchPokemon(searchInput.value));
+
 const searchPokemon = async (pokemonId = 3) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   console.log(data)
   let name = data.name;
   let height = data.height;
   let weight = data.weight;
-  let picture = data.sprites.front_default;  // data.sprites.front_default, data.sprites.back_default
-  makeDiv(name, height, weight, picture);
+  let picture = data.sprites.front_default;
+  let backPicture = data.sprites.back_default;
+  makeDiv(name, height, weight, picture, backPicture);
 };
 
 
-    const makeDiv = (name, height, weight, picture) => {
+    const makeDiv = (name, height, weight, picture, backPicture) => {
     const htmlText = `
       <div class="pokemonContainer">
         <div>Name: ${name}</div>
         <div>height: ${height}</div>
         <div>weight: ${weight}</div>
-        <div>picture: <img src="${picture}" /></div>
-      </div>
-    `;
+        <div>Pokemon Image: <br> <img src="${picture}"
+        onmouseover="this.src='${backPicture}';"
+        onmouseout="this.src='${picture}';"/></div>`;
+        // <div id="img">picture: <br> <img src="${picture}"/></div>
+        // </div>
+        // `
+    ;
     pokeDiv.innerHTML = htmlText;
   } 
 
-searchButton.addEventListener("click", () => searchPokemon(searchInput.value));
 
 
-
-
-// const nameDiv = document.createElement("div");
-  // const heightDiv = document.createElement("div");
-  // const weightDiv = document.createElement("div");
-  // const picDiv = document.createElement("div");
-  
-  // nameDiv.innerHTML = "Name: " + name;
-  // heightDiv.innerHTML = "Height: " + height;
-  // weightDiv.innerHTML = "Weight: " + weight;
-  // picDiv.innerHTML = "picture: " + picture;
-  
-  // pokeDiv.appendChild(nameDiv);
-  // pokeDiv.appendChild(heightDiv);
-  // pokeDiv.appendChild(weightDiv);
-  // pokeDiv.appendChild(picDiv);
-
-// pokemonImg.onmouseover=()=>pokemonImg.src=res.data.sprites.back_default
-// pokemonImg.onmouseout=()=>pokemonImg.src=res.data.sprites.front_default
