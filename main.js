9854972b6aced593ce1d1d952ff425efb6feb936
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-08-03 18:03:46 +0300

    add list of type names

--- main.js
+++ main.js
@@ -7,13 +7,18 @@ searchButton.addEventListener("click", () => searchPokemon(searchInput.value));
 const searchPokemon = async (pokemonId = 3) => {
   try {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
   let name = data.name;
   let height = data.height;
   let weight = data.weight;
   let picture = data.sprites.front_default;
   let backPicture = data.sprites.back_default;
-  makeDiv(name, height, weight, picture, backPicture);
+  console.log(data.types);
+  let types = [];
+  for (let i = 0; i<data.types.length; i++) {
+  types.push(data.types[i].type.name);
+  console.log(types);
+  }
+  makeDiv(name, height, weight, picture, backPicture, types);
   }
   catch (err) {
       pokeDiv.innerHTML = 
@@ -22,20 +27,26 @@ const searchPokemon = async (pokemonId = 3) => {
 };
 
 
-    const makeDiv = (name, height, weight, picture, backPicture) => {
+    const makeDiv = (name, height, weight, picture, backPicture, types) => {
     const htmlText = `
       <div class="pokemonContainer">
         <div>Name: ${name}</div>
         <div>height: ${height}</div>
         <div>weight: ${weight}</div>
-        <div>Pokemon Image: <br> <img src="${picture}"
+        <div id="img">Pokemon Image: <br> <img id="pokeImg" src="${picture}"
         onmouseover="this.src='${backPicture}';"
-        onmouseout="this.src='${picture}';"/></div>`;
-        // <div id="img">picture: <br> <img src="${picture}"/></div>
-        // </div>
-        // `
+        onmouseout="this.src='${picture}';"/></div>
+        `;
     ;
     pokeDiv.innerHTML = htmlText;
+    const ul = document.createElement("ul");
+    ul.textContent = "Types:"
+    pokeDiv.appendChild(ul);
+      for (let i = 0; i < types.length; i++) {
+          let li = document.createElement("li");
+          li.textContent = [i+1] + ". " + types[i];
+          ul.appendChild(li);
+      }
   } 
 
 
